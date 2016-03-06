22d1aaeca81600d468ec239e7801ac3ba248fa8a
laco0416 <laco0416+github@gmail.com>
2016-03-06 10:34:35 +0900

    chore(build): rename files

--- test.js
+++ test.js
@@ -1,15 +1,14 @@
 "use strict";
 
-var _assert = require("assert");
+import assert from "assert";
 
-var _assert2 = _interopRequireDefault(_assert);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-describe("tsconfig result", function () {
-  it("should match files", function () {
-    var config = require("./misc/tsconfig.json");
-    var expected = ["src/bar.ts", "src/foo.ts"];
-    _assert2.default.deepEqual(config.files, expected);
+describe("tsconfig result", () => {
+  it("should match files", () => {
+    let config = require("./misc/tsconfig.json");
+    let expected = [
+      "src/bar.ts",
+      "src/foo.ts"
+    ];
+    assert.deepEqual(config.files, expected);
   });
 });
