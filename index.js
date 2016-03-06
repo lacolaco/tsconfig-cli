ea7670e344418add556f277469acc678bac0f97d
laco0416 <laco0416+github@gmail.com>
2016-03-06 12:54:34 +0900

    refactor(core): follow to eslint

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ opt.args = program.args;
 opt.update = program.update || false;
 opt.verbose = program.verbose || false;
 
-const log = (str) => {
-  if(opt.verbose) {
+const log = str => {
+  if (opt.verbose) {
     console.log(str);
   }
 };
@@ -34,40 +34,41 @@ new Promise((resolve, reject) => {
     reject(`only 1 argument required`);
   }
   resolve();
-}).then(()=> {
+}).then(() => {
   // Check file existence
   let cfgPath = opt.args[0] || FILE_NAME;
   return new Promise((resolve, reject) => {
-    fs.stat(cfgPath, (err) => {
+    fs.stat(cfgPath, err => {
       if (err) {
         reject(`${path.resolve(cfgPath)} not exist`);
       }
       resolve(cfgPath);
     });
   });
-}).then((inputPath) => {
+}).then(inputPath => {
   log(`\tInput:\t"${inputPath}"`);
   let projectDir = path.dirname(inputPath);
   opt.outputPath = `${projectDir}/${FILE_NAME}`;
   // Load tsconfig.json
   return tsconfig
     .readFile(inputPath)
-    .then((result)=> {
+    .then(result => {
       // Resolve files into relative path
       let resolved = [];
-      result.files.forEach((file) => {
+      result.files.forEach(file => {
         resolved.push(path.relative(projectDir, file));
       });
       result.files = resolved;
       return result;
     });
-}).then((tsconfig) => {
+}).then(tsconfig => {
   // Output
+  let p;
   if (opt.update) {
     // Overwrite tsconfig.json
-    return new Promise((resolve, reject) => {
-      fs.writeFile(opt.outputPath, JSON.stringify(tsconfig, null, 4), (err) => {
-        if(err) {
+    p = new Promise((resolve, reject) => {
+      fs.writeFile(opt.outputPath, JSON.stringify(tsconfig, null, 4), err => {
+        if (err) {
           reject(err);
           return;
         }
@@ -77,11 +78,13 @@ new Promise((resolve, reject) => {
     });
   } else {
     console.log(JSON.stringify(tsconfig, null, 4));
+    p = Promise.resolve(true);
   }
-}).then(()=> {
+  return p;
+}).then(() => {
   // Completed
   log(`\tFinished!`);
-}).catch((err)=> {
+}).catch(err => {
   console.error(`[tsconfig] ${err}`);
   process.exit(1);
 });
