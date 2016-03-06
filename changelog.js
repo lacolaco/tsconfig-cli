d1372fafbea8dbac7bb8f402de82a0296500c9be
laco0416 <laco0416+github@gmail.com>
2016-03-06 11:18:07 +0900

    fix(scripts): fix changelog.js

--- changelog.js
+++ changelog.js
@@ -1,6 +1,18 @@
-const conventionalChangelog = require('conventional-changelog');
-const fs = require('fs');
+#!/usr/bin/env node
 
-conventionalChangelog({
-  preset: 'angular'
-}).pipe(fs.createWriteStream('CHANGELOG.md'));
+'use strict';
+
+const fs = require('fs');
+const cl = require('conventional-changelog');
+
+const changelogStream = fs.createWriteStream('CHANGELOG.md');
+
+const config = {
+  preset: 'angular',
+  releaseCount: 1,
+  debug: console.log.bind(console),
+};
+
+cl(config).on('error', (err) => {
+  console.error('Failed to generate changelog: ' + err);
+}).pipe(changelogStream);
